2cffb2a34899afcf65b187903df1ffe7f1e51d9d
Ariel Coppes <ariel.coppes@gemserk.com>
2011-03-16 11:05:08 -0300

    configured the manifest and changed to suffix android the helloworld

--- libgdx-test-android/src/main/java/com/gemserk/libgdx/test/HelloWorldAndroid.java
+++ libgdx-test-android/src/main/java/com/gemserk/libgdx/test/HelloWorldAndroid.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 
-public class HelloWorldApplication extends AndroidApplication {
+public class HelloWorldAndroid extends AndroidApplication {
 	@Override public void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		initialize(new HelloWorld(), false);		
