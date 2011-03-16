f15afd11e374037d2194495dfa77503bb7bbb647
Ariel Coppes <ariel.coppes@gemserk.com>
2011-03-16 10:57:27 -0300

    configured basic data for hello world applications

---
+++ libgdx-test-android/src/main/java/com/gemserk/libgdx/test/HelloWorldApplication.java
@@ -0,0 +1,12 @@
+package com.gemserk.libgdx.test;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+
+public class HelloWorldApplication extends AndroidApplication {
+	@Override public void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		initialize(new HelloWorld(), false);		
+	}
+}
