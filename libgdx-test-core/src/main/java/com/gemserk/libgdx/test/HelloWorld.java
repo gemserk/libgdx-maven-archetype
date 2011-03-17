aafcf6b4a6578e478df09e64c52472f67257ed50
Ariel Coppes <ariel.coppes@gemserk.com>
2011-03-17 12:43:38 -0300

    removed unused imports

--- libgdx-test-core/src/main/java/com/gemserk/libgdx/test/HelloWorld.java
+++ libgdx-test-core/src/main/java/com/gemserk/libgdx/test/HelloWorld.java
@@ -14,13 +14,10 @@
 package com.gemserk.libgdx.test;
 
 import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Files.FileType;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.Texture.TextureWrap;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
